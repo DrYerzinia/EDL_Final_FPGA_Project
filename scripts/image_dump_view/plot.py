ab2bb8ed2dc95805e051e5fe6471a5f1714ddc25
Michael Marques <mima3079@colorado.edu>
2019-11-17 11:08:10 -0800

    Added image dump ploter

---
+++ scripts/image_dump_view/plot.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+image = []
+
+IMAGE_WIDTH = 640
+IMAGE_HEIGHT = 480
+
+with open("image.log", "r") as image_file:
+
+    try:
+
+        image_line = []
+
+        for line in image_file:
+
+            values = line.strip().split(" ")
+            for val in values:
+
+                n_val = int(val, 16)
+
+                r = (n_val & 0x00FF0000) >> 16
+                g = (n_val & 0x0000FF00) >> 8
+                b = (n_val & 0x000000FF) >> 0
+
+                image_line.append([r, g, b])
+                
+                if len(image_line) >= IMAGE_WIDTH:
+                    image.append(image_line)
+                    image_line = []
+
+    except:
+        pass
+
+image = np.array(image)
+
+#print(np.shape(image))
+
+imgplot = plt.imshow(image)
+plt.show()
